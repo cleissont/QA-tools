b05a8fbfaf04c201dbf3bdfd0dc477841dd41382
tiagopaes <tiagopaes37@gmail.com>
2017-05-04 13:56:33 -0300

    add directory structure

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>QA Tools</title>
+</head>
+<body>
+	<h1>Welcome</h1>
+	<a href="resources/views/Auth/sign_in.php">Sign in</a>
+	<a href="resources/views/Auth/sign_up.php">Sign up</a>
+</body>
+</html>
